ed97632635b0a6486225d92089ecaf68fb4a3ea7
Rupak Khadka <rupak.fcc@gmail.com>
2022-04-12 17:07:14 +0545

    third commit

--- test/sets.py
+++ test/sets.py
@@ -39,6 +39,48 @@
 # print(thisset)
 
 
+# thisset = {"apple", "banana", "cherry"}
+# x = thisset.pop()
+# print(x)
 
 
+# The clear() method empties the set:
+# thisset = {"apple", "banana", "cherry"}
+# thisset.clear()
+# print(thisset)
 
+
+# The del keyword deletes the set completly:
+# thisset = {"apple", "banana", "cherry"}
+# del thisset
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+# for x in thisset:
+#   print(x)
+
+
+# The union() method returns a new set with all items from both sets:
+# set1 = {"a", "b" , "c"}
+# set2 = {1, 2, 3}
+# set3 = set1.union(set2)
+# print(set3)
+
+# The intersection() method will return a new set, that only contains the items that are present in both sets.
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+# z = x.intersection(y)
+# print(z)
+
+# Keep All, But NOT the Duplicates
+# The symmetric_difference_update() method will keep only the elements that are NOT present in both sets.
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+# x.symmetric_difference_update(y)
+# print(x)
+
+# The symmetric_difference() method will return a new set, that contains only the elements that are NOT present in both sets.
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.symmetric_difference(y)
+print(z)
